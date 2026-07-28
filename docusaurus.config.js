e680fe446f0318dfe7cfb1c0fd1070ce60c82724
xjmon <xjmon@users.noreply.github.com>
2026-07-28 15:24:05 +0800

    fix: commonjs config, no workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-import { themes as prismThemes } from 'prism-react-renderer';
+const { themes: prismThemes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -76,4 +76,4 @@ const config = {
   },
 };
 
-export default config;
+module.exports = config;
